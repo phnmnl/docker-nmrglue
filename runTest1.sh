0666c516b5b8bc7709582ddb9f6a30265e5c5c2e
Kristian Peters <kristian@korseby.net>
2017-03-09 09:03:42 +0100

    added fid test data

--- runTest1.sh
+++ runTest1.sh
@@ -6,12 +6,20 @@ wget -O /data/MTBLS1.zip 'https://www.ebi.ac.uk/metabolights/MTBLS1/files/MTBLS1
 # Fetch test data
 wget 'https://raw.githubusercontent.com/phnmnl/container-nmrglue/develop/test_data_spectra.csv.gz'
 wget 'https://raw.githubusercontent.com/phnmnl/container-nmrglue/develop/test_data_ppm.csv.gz'
+wget 'https://raw.githubusercontent.com/phnmnl/container-nmrglue/develop/test_data_fid.csv.gz'
 
 # Run test
 /data/runTest2.python
 
 # Compare results
 if [[ "$(cat data_spectra.csv | head -n 1)" != "$(zcat test_data_spectra.csv.gz | head -n 1)" ]]; then
+	echo "Test failed! Results do not match test data."
+	exit 1
+else
+	echo "Test succeeded successfully."
+fi
+
+if [[ "$(cat data_fid.csv | head -n 1)" != "$(zcat test_data_fid.csv.gz | head -n 1)" ]]; then
 	echo "Test failed! Results do not match test data."
 	exit 1
 else
